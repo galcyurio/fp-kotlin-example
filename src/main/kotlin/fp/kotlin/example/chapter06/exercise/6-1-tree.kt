a0376ad386e904e6d5d8d9d8af652420765901d0
galcyurio <galcyurio@gmail.com>
2020-10-08 21:34:08 +0900

    잘못 구현된 Node 수정

--- src/main/kotlin/fp/kotlin/example/chapter06/exercise/6-1-tree.kt
+++ src/main/kotlin/fp/kotlin/example/chapter06/exercise/6-1-tree.kt
@@ -15,7 +15,7 @@ sealed class Tree<out T> {
     object EmptyTree : Tree<Nothing>()
     data class Node<out T>(
         val value: T,
-        val left: Node<T>,
-        val right: Node<T>
+        val left: Tree<T>,
+        val right: Tree<T>
     ) : Tree<T>()
 }
