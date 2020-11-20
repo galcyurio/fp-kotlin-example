6e5bdd0d548e5b8bbff1f57221f7eb91e8b9ef07
galcyurio <galcyurio@gmail.com>
2020-11-21 01:18:53 +0900

    8-3 연습문제 완료

--- src/main/kotlin/fp/kotlin/example/chapter08/exercise/8_3_listApplicative.kt
+++ src/main/kotlin/fp/kotlin/example/chapter08/exercise/8_3_listApplicative.kt
@@ -2,6 +2,7 @@ package fp.kotlin.example.chapter08.exercise
 
 import fp.kotlin.example.chapter04.solution.curried
 import fp.kotlin.example.chapter07.Functor
+import io.kotlintest.shouldBe
 
 /**
  *
@@ -26,40 +27,45 @@ data class Cons<A>(val head: A, val tail: FunList<A>) : FunList<A>() {
     override fun <B> fmap(f: (A) -> B): FunList<B> = Cons(f(head), tail.fmap(f))
 }
 
-fun <A> FunList.Companion.pure(value: A): FunList<A> = TODO()
+fun <A> FunList.Companion.pure(value: A): FunList<A> = Cons(value, Nil)
 
-infix fun <A> FunList<A>.append(other: FunList<A>): FunList<A> = TODO()
+infix fun <A> FunList<A>.append(other: FunList<A>): FunList<A> = when (this) {
+    is Cons -> Cons(head, tail.append(other))
+    Nil -> other
+}
 
-infix fun <A, B> FunList<(A) -> B>.apply(f: FunList<A>): FunList<B> = TODO()
+infix fun <A, B> FunList<(A) -> B>.apply(f: FunList<A>): FunList<B> = when (this) {
+    is Cons -> f.fmap(head) append tail.apply(f)
+    Nil -> Nil
+}
 
 fun main() {
 
     val funList: FunList<(Int) -> Int> = FunList.pure { x -> x * 3 }
-    require(funList apply Cons(1, Cons(2, Cons(3, Cons(4, Nil)))) ==
-        Cons(3, Cons(6, Cons(9, Cons(12, Nil)))))
+    funList apply Cons(1, Cons(2, Cons(3, Cons(4, Nil)))) shouldBe
+        Cons(3, Cons(6, Cons(9, Cons(12, Nil))))
 
     val funList2: FunList<(Int) -> Int> =
         Cons({ it * 3 },
             Cons({ it * 10 },
                 Cons<(Int) -> Int>({ it - 2 }, Nil)))
 
-    require(funList2 apply Cons(1, Cons(2, Cons(3, Nil))) ==
+    funList2 apply Cons(1, Cons(2, Cons(3, Nil))) shouldBe
         Cons(3, Cons(6, Cons(9,
             Cons(10, Cons(20, Cons(30,
-                Cons(-1, Cons(0, Cons(1, Nil))))))))))
+                Cons(-1, Cons(0, Cons(1, Nil)))))))))
 
     val funList3: FunList<(Int) -> Int> = Nil
-    require(funList3 apply Cons(1, Cons(2, Cons(3, Cons(4, Nil)))) == Nil)
+    funList3 apply Cons(1, Cons(2, Cons(3, Cons(4, Nil)))) shouldBe Nil
 
     val funList4: FunList<(Int) -> (Int) -> Int> = FunList.pure(
         { x: Int, y: Int -> x + y }.curried())
-    require(
-        funList4
-            apply Cons(1, Cons(2, Cons(3, Nil)))
-            apply Cons(1, Cons(2, Cons(3, Nil)))
-            ==
-            Cons(2, Cons(3, Cons(4,
-                Cons(3, Cons(4, Cons(5,
-                    Cons(4, Cons(5, Cons(6, Nil))))))))))
+    (funList4
+        apply Cons(1, Cons(2, Cons(3, Nil)))
+        apply Cons(1, Cons(2, Cons(3, Nil)))
+        shouldBe
+        Cons(2, Cons(3, Cons(4,
+            Cons(3, Cons(4, Cons(5,
+                Cons(4, Cons(5, Cons(6, Nil))))))))))
 }
 
